a6fa82fd21a7ff40467aa68e5a8f4a8592cddce0
Dmitry <free-wind-2012@yandex.ru>
2020-08-01 14:44:22 +0300

    Добавлено 2 дз

--- src/main/java/ru/geekbrains/sample/controllers/MainController.java
+++ src/main/java/ru/geekbrains/sample/controllers/MainController.java
@@ -1,11 +1,15 @@
 package ru.geekbrains.sample.controllers;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.geekbrains.sample.dto.Student;
+import ru.geekbrains.sample.repository.StudentsRepository;
 
 @Controller
 public class MainController {
@@ -15,16 +19,31 @@ public class MainController {
       return "index";
    }
 
+   StudentsRepository studentsRepository;
+
+    @GetMapping("/studentsList")
+    public String getStudentList(Model model) {
+        model.addAttribute("students",studentsRepository.findAll());
+        return "studentList";
+    }
+
+    @GetMapping("/profiles")
+    public String getProfile(Model model, @RequestParam Long id) {
+        model.addAttribute("profiles",studentsRepository.findById(id));
+        return "profile";
+    }
+
     @GetMapping("/students")
-    public String getStudentPage() {
-        //чтобы вывести список студентов нужно добавить бин StudentsRepository положить данные в Model
+    public String getStudent() {
+//        чтобы вывести список студентов нужно добавить бин StudentsRepository положить данные в Model
         return "student";
     }
 
     @PostMapping("/students")
     public String sendForm(@ModelAttribute Student student) {
+        studentsRepository.init();
+        studentsRepository.saveOrUpdateStudent(student);
         System.out.println(student);
-        return "redirect:/";
+        return "studentList";
     }
-
 }
