9cce4a3b87fab3fc5b8ecccd22ec77fb4d2eda9f
Dmitry <free-wind-2012@yandex.ru>
2020-08-18 20:11:33 +0300

    add hw 7

--- src/main/java/ru/geekbrains/sample/controllers/ItemController.java
+++ src/main/java/ru/geekbrains/sample/controllers/ItemController.java
@@ -5,45 +5,47 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.geekbrains.sample.dto.BookDTO;
+import ru.geekbrains.sample.dto.ItemDTO;
 import ru.geekbrains.sample.services.BookService;
+import ru.geekbrains.sample.services.ItemService;
 
 import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/books")
-public class BookController {
+@RequestMapping("/items")
+public class ItemController {
 
-    public final BookService bookService;
+    public final ItemService itemService;
 
     @GetMapping
     public String getIndexPage(Model model) {
-        model.addAttribute("books", bookService.getAllBooks());
+        model.addAttribute("item", itemService.getAllItem());
         return "index";
     }
 
     @GetMapping("/{id}")
     public String getBookPage(@PathVariable String id, Model model) {
-        model.addAttribute("book", bookService.getOne(UUID.fromString(id)));
-        return "book";
+        model.addAttribute("item", itemService.getOne(UUID.fromString(id)));
+        return "item";
     }
 
     @PostMapping
-    public String postBookPage(@RequestBody BookDTO bookDTO) {
-        bookService.postOne(bookDTO);
-        return "book";
+    public String postBookPage(@RequestBody ItemDTO itemDTO) {
+        itemService.postOne(itemDTO);
+        return "item";
     }
 
     //изменить
     @PutMapping("/{id}")
-    public String putBookPage(@RequestBody BookDTO bookDTO, @RequestBody String id) {
-        bookService.put(bookDTO, UUID.fromString(id));
-        return "book";
+    public String putBookPage(@RequestBody ItemDTO itemDTO, @RequestBody String id) {
+        itemService.put(itemDTO, UUID.fromString(id));
+        return "item";
     }
 
     @DeleteMapping("/{id}")
     public String deleteBookPage(@RequestBody String id) {
-        bookService.delete(UUID.fromString(id));
-        return "book";
+        itemService.delete(UUID.fromString(id));
+        return "item";
     }
 }
--- src/main/java/ru/geekbrains/sample/services/BookService.java
+++ src/main/java/ru/geekbrains/sample/services/BookService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
 
+import ru.geekbrains.sample.dto.BookDTO;
 import ru.geekbrains.sample.persistence.entities.Book;
 import ru.geekbrains.sample.persistence.entities.School;
 import ru.geekbrains.sample.persistence.repositories.BookRepository;
@@ -18,9 +19,6 @@ public class BookService {
 
     private final BookRepository bookRepository;
 
-    private final SchoolRepository schoolRepository;
-
-
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
     }
@@ -29,4 +27,26 @@ public class BookService {
         return bookRepository.findById(id).orElse(null);
     }
 
+    public void postOne(BookDTO bookDTO) {
+        bookRepository.save(Book.builder()
+                .available(bookDTO.isAvailable())
+                .name(bookDTO.getName())
+                .created(bookDTO.getCreated())
+                .description(bookDTO.getDescription())
+                .build());
+    }
+
+    public void put(BookDTO bookDTO, UUID fromString) {
+        bookRepository.deleteById(fromString);
+        bookRepository.save(Book.builder()
+                .available(bookDTO.isAvailable())
+                .name(bookDTO.getName())
+                .created(bookDTO.getCreated())
+                .description(bookDTO.getDescription())
+                .build());
+    }
+
+    public void delete(UUID fromString) {
+        bookRepository.deleteById(fromString);
+    }
 }
