5273dc03c9a671984896a4e12ef4a85f199642ad
Dmitry <free-wind-2012@yandex.ru>
2020-08-21 18:57:55 +0300

    Add hw lesson 8

--- src/main/java/ru/geekbrains/sample/controllers/ShopController.java
+++ src/main/java/ru/geekbrains/sample/controllers/ShopController.java
@@ -7,6 +7,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import ru.geekbrains.sample.dto.v2.ShopuserDTO;
 import ru.geekbrains.sample.persistence.entities.Shopuser;
 import ru.geekbrains.sample.services.BookService;
 import ru.geekbrains.sample.services.SchoolService;
@@ -44,10 +47,19 @@ public class ShopController {
 
         Shopuser shopuser = shopuserService.findByLogin(principal.getName());
 
+        model.addAttribute("userList", shopuserService.findAll());
+
         model.addAttribute("shopuser", shopuser);
 
         return "profile";
 
+    }
+
+    @Secured({"ROLE_ADMIN"})
+    @PostMapping("/profile")
+    public String postBookPage(@RequestBody ShopuserDTO shopuserDTO) {
+        shopuserService.saveOneUser(shopuserDTO);
+        return "profile";
     }
 
 
--- src/main/java/ru/geekbrains/sample/services/ShopuserService.java
+++ src/main/java/ru/geekbrains/sample/services/ShopuserService.java
@@ -9,12 +9,17 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import org.springframework.stereotype.Service;
+import ru.geekbrains.sample.dto.v2.BookDTOV2;
+import ru.geekbrains.sample.dto.v2.ShopuserDTO;
+import ru.geekbrains.sample.persistence.entities.Book;
 import ru.geekbrains.sample.persistence.entities.Shopuser;
 import ru.geekbrains.sample.persistence.enums.Role;
 import ru.geekbrains.sample.persistence.repositories.ShopuserRepository;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +29,10 @@ public class ShopuserService implements UserDetailsService {
 
     public Shopuser findByLogin(String login) {
         return shopUserRepository.findByLogin(login);
+    }
+
+    public List<Shopuser> findAll() {
+        return shopUserRepository.findAll();
     }
 
     @Override
@@ -36,8 +45,18 @@ public class ShopuserService implements UserDetailsService {
     }
 
     private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Role role) {
-        return role != null ? new ArrayList<GrantedAuthority>() {{ add((GrantedAuthority) role::name); }} : new ArrayList<>();
+        return role != null ? new ArrayList<GrantedAuthority>() {{
+            add((GrantedAuthority) role::name);
+        }} : new ArrayList<>();
     }
 
+    public void saveOneUser(ShopuserDTO shopuserDTO) {
+        shopUserRepository.save(Shopuser.builder()
+                .login(shopuserDTO.getLogin())
+                .password(shopuserDTO.getPassword())
+                .firstName(shopuserDTO.getFirstName())
+                .role(shopuserDTO.getRole())
+                .build());
+    }
 
 }
